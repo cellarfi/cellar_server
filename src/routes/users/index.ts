6579a0ad7f966571272dada84b09dc956e57d58b
Leeyah-123 <junaidaaliyah260@gmail.com>
2025-07-01 15:55:19 +0100

    fix: Fix user routes unauthorized

--- src/routes/users/index.ts
+++ src/routes/users/index.ts
@@ -4,13 +4,14 @@ import {
   getProfile,
   updateProfile,
 } from '@/controllers/usersController';
+import { authMiddleware } from '@/middleware/auth.middleware';
 import { Router } from 'express';
 
 const router = Router();
 
-router.get('/me', getProfile);
-router.post('/', createUser);
-router.patch('/me', updateProfile);
-router.delete('/me', deleteUser);
+router.get('/me', authMiddleware, getProfile);
+router.post('/', authMiddleware, createUser);
+router.patch('/me', authMiddleware, updateProfile);
+router.delete('/me', authMiddleware, deleteUser);
 
 export default router;
