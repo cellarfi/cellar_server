22fe4ed42ab1f2bb565943b6df3c9e25dc8965c4
Leeyah-123 <junaidaaliyah260@gmail.com>
2025-07-11 11:38:13 +0100

    feat: Implement check if tag name exists endpoint

--- src/routes/users/index.ts
+++ src/routes/users/index.ts
@@ -1,17 +1,19 @@
 import {
+  checkIfTagNameExists,
   createUser,
   deleteUser,
   getProfile,
   updateProfile,
-} from '@/controllers/usersController'
-import { authMiddleware } from '@/middleware/auth.middleware'
-import { Router } from 'express'
+} from '@/controllers/usersController';
+import { authMiddleware } from '@/middleware/auth.middleware';
+import { Router } from 'express';
 
-const router = Router()
+const router = Router();
 
-router.get('/me', authMiddleware, getProfile)
-router.post('/', authMiddleware, createUser)
-router.patch('/me', authMiddleware, updateProfile)
-router.delete('/me', authMiddleware, deleteUser)
+router.get('/me', authMiddleware, getProfile);
+router.get('/exists/tag_name/:tag_name', checkIfTagNameExists);
+router.post('/', authMiddleware, createUser);
+router.patch('/me', authMiddleware, updateProfile);
+router.delete('/me', authMiddleware, deleteUser);
 
-export default router
+export default router;
