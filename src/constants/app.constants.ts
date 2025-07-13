57341818b9ceb2efc1c36d6a3978c678509c06f7
Leeyah-123 <junaidaaliyah260@gmail.com>
2025-07-13 12:04:56 +0100

    feat: add wallet management and tag name update restrictions

---
+++ src/constants/app.constants.ts
@@ -0,0 +1,7 @@
+// Time limits in milliseconds
+export const TAG_NAME_UPDATE_TIME_LIMIT = 7 * 24 * 60 * 60 * 1000; // 7 days in milliseconds
+
+// Error messages
+export const ERROR_MESSAGES = {
+  TAG_NAME_UPDATE_LIMIT: 'You can only update your tag name once every 7 days',
+};
